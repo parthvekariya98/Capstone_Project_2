cd740b0e8d505a2cf5c6ca1f8243d578b1c83ee9
Parth Vekariya <parthvekariya260898@gmail.com>
2023-04-03 16:14:40 -0400

    - project setup for HomeSpotter(React Native)

---
+++ HomeSpotter/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>HomeSpotter</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
+
+export default App;
