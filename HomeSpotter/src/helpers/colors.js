7173ce9007e1719e56c2068a4a0a42d3351137cf
Parth Vekariya <parthvekariya260898@gmail.com>
2023-04-15 01:58:51 -0400

    - ui design for welcome screen

---
+++ HomeSpotter/src/helpers/colors.js
@@ -0,0 +1,10 @@
+export default colors = {
+    app_color: '#5863F7',
+    secondary: '#3B3B3B',
+    white: '#FFFFFF',
+    black: '#000000',
+    gray: '#BEBEBE',
+    lightGray: '#F5F5F5',
+    darkGray: '#A9A9A9',
+};
+  
--- HomeSpotter/src/screens/WelcomeScreen.js
+++ HomeSpotter/src/screens/WelcomeScreen.js
@@ -1,24 +1,121 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet, Dimensions } from 'react-native';
+import React from 'react';
+import { View, TouchableOpacity, Text, Image, StyleSheet } from 'react-native';
+import colors from '../helpers/colors';
 
-const { width, height } = Dimensions.get('window');
-
-const WelcomeScreen = ({ navigation }) => {
-
-	return (
-		<View style={styles.container}>
-			<Text style={styles.title}>Welcome</Text>
-		</View>
-	);
+const WelcomeScreen = () => {
+    return (
+        <View style={styles.container}>
+            <View style={styles.topView}>
+            <Text style={styles.firstText}>Welcome to</Text>
+            <Text style={styles.secondText}>HomeSpotter</Text>
+            <Text style={styles.thirdText}>A place where you can find your next Home...</Text>
+            <Text style={styles.letText}>Let’s Get Started...</Text>
+            </View>
+            <TouchableOpacity
+                style={styles.button}
+                onPress={() => { }}
+            >
+                <View style={styles.buttonContent}>
+                    <Image
+                        source={require('../assets/images/ic_google.png')}
+                        style={styles.icon}
+                    />
+                    <Text style={styles.buttonText}>Sign Up with Google</Text>
+                </View>
+            </TouchableOpacity>
+            <TouchableOpacity
+                style={styles.button}
+                onPress={() => { }}
+            >
+                <View style={styles.buttonContent}>
+                    <Image
+                        source={require('../assets/images/ic_email.png')}
+                        style={styles.icon}
+                    />
+                    <Text style={styles.buttonText}>Sign Up with Email</Text>
+                </View>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => { }}>
+                <Text style={styles.alText}>Already have an account? <Text style={styles.loginText}>Login</Text></Text>
+            </TouchableOpacity>
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		alignItems: 'center',
-		justifyContent: 'center',
-		alignContent: 'center'
-	}
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'white'
+    },
+    topView: {
+        justifyContent: 'flex-start',
+        alignItems: 'flex-start',
+        width: '90%',
+        marginBottom: 15
+    },
+    button: {
+        backgroundColor: 'white',
+        borderColor: 'black',
+        borderRadius: 10,
+        borderWidth: 1,
+        paddingVertical: 15,
+        paddingHorizontal: 20,
+        marginVertical: 8,
+        width: '90%',
+        marginHorizontal: '5%',
+    },
+    buttonContent: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent:'center'
+    },
+    buttonText: {
+        fontSize: 17,
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+    firstText: {
+        fontSize: 24,
+        fontWeight: 'normal',
+        textAlign: 'center',
+    },
+    secondText: {
+        fontSize: 45,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        color: colors.app_color
+    },
+    thirdText: {
+        fontSize: 17,
+        fontWeight: '300',
+        textAlign: 'center',
+    },
+    letText: {
+        fontSize: 17,
+        fontWeight: '300',
+        textAlign: 'center',
+        marginTop: '15%'
+    },
+    alText: {
+        fontSize: 17,
+        fontWeight: '300',
+        textAlign: 'center',
+        marginTop: 15
+    },
+    loginText: {
+        fontSize: 17,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        color: colors.app_color,
+        textDecorationLine: 'underline',
+    },
+    icon: {
+        width: 24,
+        height: 24,
+        marginRight: 10,
+    },
 });
 
 export default WelcomeScreen;
